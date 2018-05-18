04867f766baae11a30f262049775a5395ca12528
Malte Lantin <malantin@microsoft.com>
2018-05-18 12:55:05 +0200

    Add the project and docu to the repository
    Add the middleware project, the demo project and the readme to the repository. The middleware can be found in the .Net standard project APIKeyMiddleware

---
+++ APIKeyMiddleware/APIKeyMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace Microsoft.AspNetCore.Builder.CustomMiddleware
+{
+    class APIKeyMiddleware
+    {
+        // Set or retrieve you api key name that is expected in the request header
+        private string APIKeyHeaderName { get { return "x-company-apikey"; } }
+        // Set or retrieve the api key you expect. This could a share or individual key for each user
+        private string APIKey { get { return "<yourkey>"; } }
+
+        private readonly RequestDelegate _next;
+
+        public APIKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.Keys.Contains(APIKeyHeaderName))
+            {
+                // The header has not been set correctly. Return 400: Bad request.
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync($"Error 400: Bad request. Please set {APIKeyHeaderName} header and the correct API key.");
+                return;
+            }
+
+            if (!context.Request.Headers[APIKeyHeaderName].Equals(APIKey))
+            {
+                // The api key was incorrect. Return 401: Unauthoried.
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Error 401: Unauthorized. Incorrect or no API key.");
+                return;
+            }
+
+            // Call the next delegate/middleware in the pipeline
+            await this._next(context);
+        }
+    }
+
+    public static class APIKeyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAPIKey(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<APIKeyMiddleware>();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,32 @@
+# A simple middleware to check for API Keys in your ASP.Net Core APIs
+
+This is a simple ASP.Net middleware you can use to check for API keys in each request to you services. It is intended to be used in a microservice architecture where each (group of) service(s) is a seperate project.
+
+Just configure the middleware in your Startup.cs:
+
+```csharp
+public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        // Check for a valid API key using the API Key Middleware
+        app.UseAPIKey();
+
+        app.UseMvc();
+        }
+```
+
+Try the demo project. When authorized you will get a correct response:
+
+![A sample request through Postman, returning the 200 OK and correct response when authorized](./Images/demo_postman.png)
+
+Otherwise you will get a status code 400 or 401:
+
+![A sample request through Postman, returning the 401 Unauthorized response when not authorized](./Images/demo_postman_2.png)
+
+Just modify the project for you convenience. 
+
+**The API key should not be stored in your code for production applications!**
